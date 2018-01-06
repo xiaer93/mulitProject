ae397025fac8f91003e129e33034e382e8fd98bb
xiaer93 <happy_cjw@qq.com>
2018-01-06 16:00:39 +0800

    animation

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports={
+
+    entry:{
+        animation:'./animationBykeyframe/util/index.js'
+    },
+    output:{
+        path:__dirname+'/build',
+        filename:'[name].js'
+    }
+};
